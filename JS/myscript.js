b4c10ebbf8fe6a6a185fd0ccc9d7ee7a1b7c4f94
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-19 17:01:46 +0200

    feat: add discount variables

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,5 +1,15 @@
 //variabile km
-const  passengerkm = parseInt(prompt ("quanti km percorrerai?"))
+//const  passengerkm = parseInt(prompt ("quanti km percorrerai?"))
 
 //variabile età
-const passengerage = parseInt(prompt("quanti anni hai?"))
+//const passengerage = parseInt(prompt("quanti anni hai?"))
+
+//variabile sconto minorenni
+const underagediscount = (20 / 100)
+
+//variabile sconto over 65
+const overagediscount = (40 / 100)
+
+
+
+
