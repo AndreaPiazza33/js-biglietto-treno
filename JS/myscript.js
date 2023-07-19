18fb4fe402a2b37417feed442c3c83bef6603487
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-19 17:28:12 +0200

    feat: add ticket price conditional statement

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,8 +1,8 @@
 //variabile km
-//const  passengerkm = parseInt(prompt ("quanti km percorrerai?"))
+const  passengerkm = parseInt(prompt ("quanti km percorrerai?"))
 
 //variabile età
-//const passengerage = parseInt(prompt("quanti anni hai?"))
+const passengerage = parseInt(prompt("quanti anni hai?"))
 
 //variabile sconto minorenni
 const underagediscount = (20 / 100)
@@ -10,6 +10,20 @@ const underagediscount = (20 / 100)
 //variabile sconto over 65
 const overagediscount = (40 / 100)
 
+//variabile costo al km
+const kmprice = (0.21)
 
+//condizioni per lo sconto
+if(passengerage < 18){
+    x = underagediscount
+}
+else if (passengerage >= 65) {
+    x = overagediscount
+}
+else {
+    x = 1
+}
 
+//variabile prezzo finale
+const ticketprice = ((kmprice * passengerkm) * x)
 
